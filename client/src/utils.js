4288c85946380482aed573e706b6ee1286fb9311
jjwarren44 <jjwarren44@gmail.com>
2021-10-03 10:56:39 -0400

    updated metamask connection

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,26 +1,22 @@
 import Web3 from "web3";
 import Wallet from "./contracts/Wallet.json";
+import detectEthereumProvider from "@metamask/detect-provider";
 
-const getWeb3 = () => {
-    return new Promise((resolve, reject) => {
-        window.addEventListener("load", async() => {
-            // check if metamask wallet is present in browser
-            if (window.ethereum) {
+const getWeb3 = () =>
+    new Promise(async(resolve, reject) => {
+        let provider = await detectEthereumProvider();
+        if (provider) {
+            await provider.request({ method: "eth_requestAccounts" });
+            try {
                 const web3 = new Web3(window.ethereum);
-                try {
-                    await window.ethereum.enable();
-                    resolve(web3);
-                } catch (error) {
-                    reject(error);
-                }
-            } else if (window.web3) {
-                resolve(window.web3);
-            } else { // no metamask installed
-                reject("Must install metamask");
+                resolve(web3);
+            } catch (error) {
+                reject(error);
             }
-        });
+        }
+        reject("Install Metamask");
     });
-};
+
 
 const getWallet = async web3 => {
     const networkId = await web3.eth.net.getId();
